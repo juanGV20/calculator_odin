3bb9b7c2b1f20867419ed02729674edfdfbdcb45
juanGV20 <j99600@hotmail.com>
2026-02-03 19:02:27 -0500

    add functions for basic operations
    Signed-off-by: juanGV20 <j99600@hotmail.com>
    
     Changes to be committed:
    	modified:   app.js

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log('Hello odin');
+let number_A;
+let number_B;
+let operator;
+
+function add (number_a, number_b){
+    return number_a + number_b;
+}
+
+function subtract (number_a, number_b) {
+    return number_a - number_b;
+}
+
+function multiply (number_a , number_b){
+    return number_a * number_b;
+} 
+
+function divide (number_a, number_b){
+    return number_a / number_b;
+}
+
+function operate (number_a, number_b, operator) {
+    let result = 0;
+    switch(operator){
+        case '+':
+            result = add(number_a, number_b);
+        break;
+        
+        case '-':
+            result = subtract(number_a, number_b);
+        break;
+        
+        case '*': 
+            result = multiply(number_a, number_b);
+        break;
+        
+        case '/':
+            result = divide(number_a, number_b);
+        break;
+        
+        default:
+            console.log('Operador todavia no disponible');
+    }
+    return result;
+}
+
+console.log(operate(3,2,'*'));
